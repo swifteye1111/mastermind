8e08d98cdd48475670c52d74ccf31fcdf8a7492c
swifteye1111 <hasak.josh@gmail.com>
2022-09-26 16:45:43 +0100

    Clean up code.

--- mastermind.rb
+++ mastermind.rb
@@ -8,10 +8,20 @@ end
 # Game
 class Game
   def initialize
-    if select_game == 'breaker'
+    playing = true
+    while playing
+      if select_game == 'breaker'
       PlayBreaker.new(Player.new, CompCodeMaker.new)
-    else
+      else
       PlayMaker.new(Player.new, CompCodeBreaker.new)
+      end
+      puts 'Play again? Y/n'
+      again = gets.chomp.downcase
+      until ['y','n'].include?(again)
+        puts 'Please input "Y" or "n":'
+        again = gets.chomp.downcase
+      end
+      playing = false if again == 'n'
     end
   end
 
